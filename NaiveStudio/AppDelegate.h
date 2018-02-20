a4e551be0d049e87b97e436308a8dd66f16f3468
Liang <ogreaxe1@gmail.com>
2018-02-20 09:08:20 +0800

    first commit

---
+++ NaiveStudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NaiveStudio
+//
+//  Created by Liang,Zhiyuan(GIS) on 2018/2/16.
+//  Copyright © 2018年 Liang,Zhiyuan(GIS). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NaiveStudio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NaiveStudio
+//
+//  Created by Liang,Zhiyuan(GIS) on 2018/2/16.
+//  Copyright © 2018年 Liang,Zhiyuan(GIS). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
